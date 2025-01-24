75cb1e014f2ca0b139580f58884084425f8888e4
kavi castelo <kavindu123450@gmail.com>
2025-01-24 21:45:51 +0530

    directives and states

--- task-tracker/src/app/app.routes.server.ts
+++ task-tracker/src/app/app.routes.server.ts
@@ -3,6 +3,9 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: (): Promise<Record<string, string>[]> => {
+      return Promise.resolve([]);
+    }
   }
 ];
